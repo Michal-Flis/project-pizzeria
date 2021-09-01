c27ae629f512369f089562fe6c0db1895d1751cc
Michał Flis <michal.flis@op.pl>
2021-09-01 15:00:39 +0200

    If

--- src/js/script.js
+++ src/js/script.js
@@ -81,17 +81,19 @@
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-    const clickableTrigger = ???;
+    const clickableTrigger = document.querySelector(select.menuProduct.clickable);
      /* START: add event listener to clickable trigger on event click */
      clickableTrigger.addEventListener('click', function(event) {
     /* prevent default action for event */
       event.preventDefault();
       /* find active product (product that has active class) */
-      const productActive = document.querySelector(select.all.menuProductsActive);
+      const activeProduct = document.querySelector(select.all.menuProductsActive);
       /* if there is active product and it's not thisProduct.element, remove class active from it */
-
+      if(activeProduct !== thisProduct && activeProduct !== null){
+        activeProduct.classList.remove(select.menuProduct.clickable)
+      };
       /* toggle active class on thisProduct.element */
-      
+      thisProduct.element.classList.toggle(select.menuProduct.clickable);
      });
     }
     
